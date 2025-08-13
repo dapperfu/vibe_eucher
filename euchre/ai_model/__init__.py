78aea37e8552d4824a635d6d803395ece4bc0cf6
Frey, Jed <dapperfu@users.github.com>
2025-08-13 13:00:18 -0400

    .

--- euchre/ai_model/__init__.py
+++ euchre/ai_model/__init__.py
@@ -3,11 +3,8 @@
 from .model_player import ModelPlayer
 from .euchre_nn import EuchreNN
 from .game_state_encoder import GameStateEncoder
-from .training_pipeline import TrainingPipeline
-
 __all__ = [
     'ModelPlayer', 
     'EuchreNN',
-    'GameStateEncoder', 
-    'TrainingPipeline'
+    'GameStateEncoder'
 ] 
